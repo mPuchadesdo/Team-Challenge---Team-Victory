dfc593f3214128d7f03c07b602d9e50e66a95bbd
JBC2024 <jbarcoc@gmail.com>
2024-11-08 10:11:54 +0100

    Update variables.py
    Definición inicial de constantes

---
+++ variables.py
@@ -0,0 +1,14 @@
+#Dimension de lado del tablero
+TABLERO_LONGITUD = 10
+
+#Caracter para posición de agua sin disparo
+CARACTER_AGUA = " "
+
+#Caracter para posición de barco sin disparo
+CARACTER_BARCO = " "
+
+#Caracter para posición de barco con disparo
+CARACTER_DISPARO_OK = "*"
+
+#Caracter para posición agua con disparo
+CARÁCTER_DISPARO_NOK = "-"
